c7c9797a7f4690e3e6a10b12d706a0eba114e9ea
Anna Busby <anna.busby.services@outlook.com>
2023-12-21 15:36:00 +0000

    Showing different sheets and dismissing them

--- iExpense/Practice.swift
+++ iExpense/Practice.swift
@@ -10,21 +10,45 @@ import SwiftUI
 @Observable
 class User {
    var firstName = "Bilbo"
-    var lastName = "Baggin"
+    var lastName = "Baggins"
 }
 
 struct Practice: View {
     
     @State private var user = User()
     
+    @State private var isShowing = false
+    
     var body: some View {
         Text("Hello, your name is \(user.firstName) \(user.lastName)")
         
         TextField("Enter first name:", text: $user.firstName)
         TextField("Enter last name:", text: $user.lastName)
+        
+        Button("Show next sheet") {
+            isShowing.toggle()
+        }
+        .sheet(isPresented: $isShowing) {
+            Practice2(name: "\(user.firstName) \(user.lastName)")
+        }
     }
+        
 }
+   
 
+struct Practice2: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var name: String
+    
+    var body: some View {
+        Text("Hello \(name)")
+        Button ("Goodbye") {
+            dismiss()
+        }
+    }
+    
+}
 #Preview {
     Practice()
 }
