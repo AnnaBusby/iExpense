316a4a750f5e2319e9f56a1c86194b29d1d52ff5
Anna Busby <anna.busby.services@outlook.com>
2023-12-21 15:58:19 +0000

    Adding & deleting rows in a list

--- iExpense/Practice.swift
+++ iExpense/Practice.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+//This makes the class watch for changes to the variables inside the class
 @Observable
 class User {
    var firstName = "Bilbo"
@@ -19,6 +20,9 @@ struct Practice: View {
     
     @State private var isShowing = false
     
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
     var body: some View {
         Text("Hello, your name is \(user.firstName) \(user.lastName)")
         
@@ -28,27 +32,60 @@ struct Practice: View {
         Button("Show next sheet") {
             isShowing.toggle()
         }
+        //when the $isShowing var is true then show a new sheet
         .sheet(isPresented: $isShowing) {
+            //This is the sheet/view to show, and weve passed it some paramenters from the current view
             Practice2(name: "\(user.firstName) \(user.lastName)")
         }
+        
+        //This next bit is focusing on creating rows and deleting them
+        NavigationStack{
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    //when we swipe to delete run the function remove rows, which will delete the selected row from view and also from the numbers array
+                    .onDelete(perform: removeRows)
+                }
+                
+                
+                Button("Add new number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
         
 }
    
 
 struct Practice2: View {
+    //This allows the user to dismis the current view depending on how it was created
     @Environment(\.dismiss) var dismiss
     
     var name: String
     
     var body: some View {
         Text("Hello \(name)")
+        
+        //This button initates the dismiss action
         Button ("Goodbye") {
             dismiss()
         }
     }
     
 }
+
+
 #Preview {
     Practice()
 }
