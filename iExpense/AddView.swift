0de1700d3989db6144a07bdef80d64cfed36b2a9
Anna Busby <anna.busby.services@outlook.com>
2023-12-24 11:52:31 +0000

    Added a new sheet for adding new expenses. It pulls data from the content view but doesnt pass anything back yet

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Anna Busby on 24/12/2023.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
